f17498111d31c4bda12fdf8d1a630b63b76eaf13
unknown <peter.codecool@gmail.com>
2017-06-15 03:20:34 +0200

    all executed

---
+++ src/main/java/com/codecool/jpaexample/CCLocation.java
@@ -0,0 +1,7 @@
+package com.codecool.jpaexample;
+
+public enum CCLocation {
+    Miskolc,
+    Budapest,
+    Krakow;
+}
--- src/main/java/com/codecool/jpaexample/model/Klass.java
+++ src/main/java/com/codecool/jpaexample/model/Klass.java
@@ -1,17 +1,39 @@
 package com.codecool.jpaexample.model;
 
+import com.codecool.jpaexample.CCLocation;
+import org.hibernate.annotations.*;
+
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="Class")
 public class Klass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    private CCLocation location;
+
+    @OneToMany(mappedBy = "")
     private Set<Student> students = new HashSet<>();
 
     public Klass() {}
 
     public Klass(String name) {
         this.name = name;
+    }
+
+    public Klass(String name, CCLocation location) {
+        this(name);
+        this.location = location;
     }
 
     public String getName() {
